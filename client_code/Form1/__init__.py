569bd26de67a7ad354fac639ff76ed2ae85fd26a
stone.john@mac.com <stone.john@mac.com>
2024-03-08 13:53:55 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+# Any code you write here will run when the form opens.
+    anvil.users.login_with_form()
+    print(f'This is a test to see if this is triggered when a user is attempting to get a password')
+    print(f"This user has logged in: {anvil.users.get_user()['email']}")
+    #anvil.server.call('say_hello', 'Anvil Developer')
