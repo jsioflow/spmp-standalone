811252f2ece60c5faf36b596a97fb91ead4c5f65
stone.john@mac.com <stone.john@mac.com>
2024-03-08 13:59:24 +0000

    Edited settings, ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,10 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
+import anvil.users
 
 class Form1(Form1Template):
   def __init__(self, **properties):
