372044cb70d4442877c02ef6732c1c3ee1584008
stone.john@mac.com <stone.john@mac.com>
2024-03-09 21:05:10 +0000

    Edited ServerModule1 and Settings

--- client_code/Settings/__init__.py
+++ client_code/Settings/__init__.py
@@ -6,6 +6,15 @@ from anvil.tables import app_tables
 import anvil.server
 import anvil.users
 
+def record_parameter(dict, parameter, value):
+    dict[parameter] = value
+    print(f"Parameter '{parameter}' with value '{value}' recorded successfully.")
+
+def display_parameters(dict):
+    print("\nList of Recorded Parameters:")
+    for parameter, value in dict.items():
+        print(f"{parameter}: {value}")
+
 class Settings(SettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -24,10 +33,43 @@ class Settings(SettingsTemplate):
 
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
+    parameter1 = anvil.server.call('get_config_parameter', 'ip_address')
+    self.text_box_1.text = parameter1
+    parameter2 = anvil.server.call('get_config_parameter', 'customer_id')
+    self.text_box_2.text = parameter2
+    parameter3 = anvil.server.call('get_config_parameter', 'battery_capacity')
+    self.text_box_3.text = parameter3
+    parameter4 = anvil.server.call('get_config_parameter', 'location_region')
+    self.text_box_4.text = parameter4
+    parameter5 = anvil.server.call('get_config_parameter', 'location_name')
+    self.text_box_5.text = parameter5
+    parameter6 = anvil.server.call('get_config_parameter', 'timezone')
+    self.text_box_6.text = parameter6
+    parameter7 = anvil.server.call('get_config_parameter', 'battery_max_level')
+    self.text_box_7.text = parameter7
+    parameter8 = anvil.server.call('get_config_parameter', 'latitude')
+    self.text_box_8.text = parameter8
+    parameter9 = anvil.server.call('get_config_parameter', 'longitude')
+    self.text_box_9.text = parameter9
+    parameter10 = anvil.server.call('get_config_parameter', 'max_charge_w')
+    self.text_box_10.text = parameter10
     pass
 
   def outlined_button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
+    parameter_dict = {}
+    record_parameter(parameter_dict, "ip_address", self.text_box_1.text)
+    record_parameter(parameter_dict, "customer_id", self.text_box_2.text)
+    record_parameter(parameter_dict, "battery_capacity", self.text_box_3.text)
+    record_parameter(parameter_dict, "location_region", self.text_box_4.text)
+    record_parameter(parameter_dict, "location_name", self.text_box_5.text)
+    record_parameter(parameter_dict, "timezone", self.text_box_6.text)
+    record_parameter(parameter_dict, "battery_max_level", self.text_box_7.text)
+    record_parameter(parameter_dict, "latitude", self.text_box_8.text)
+    record_parameter(parameter_dict, "longitude", self.text_box_9.text)
+    record_parameter(parameter_dict, "max_charge_w", self.text_box_10.text)
+    display_parameters(parameter_dict)
+    anvil.server.call('save_configuration', parameter_dict)
     pass
 
    
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -7,6 +7,51 @@ import os
 import csv
 import sqlite3
 
+
+def read_csv_to_dict(file_path):
+    data_dict = {}
+    
+    with open(file_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        
+        # Assuming the first row contains header keys
+        headers = next(reader, None)
+        
+        if headers:
+            for row in reader:
+                # Use a dictionary comprehension to create the dictionary
+                data_dict = {headers[i]: row[i] for i in range(min(len(headers), len(row)))}
+    
+    return data_dict
+    
+def write_dict_to_csv(file_path, data_dict):
+    # Extract keys and values from the dictionary
+    keys = list(data_dict.keys())
+    values = list(data_dict.values())
+
+    # Write the dictionary to a CSV file
+    with open(file_path, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(keys)  # Write header
+        writer.writerow(values)  # Write values
+
+def display_parameters(dict):
+    print("\nList of Recorded Parameters:")
+    for parameter, value in dict.items():
+        print(f"{parameter}: {value}")
+        
+def pick_parameter_value(dict, parameter):
+    try:
+        selected_value = dict[parameter]
+        print(f"Associated Value for '{parameter}': {selected_value}")
+        return selected_value
+    except KeyError:
+        print(f"Parameter '{parameter}' not found. Please choose a valid parameter.")
+
+def record_parameter(dict, parameter, value):
+    dict[parameter] = value
+    print(f"Parameter '{parameter}' with value '{value}' recorded successfully.")
+
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
@@ -14,9 +59,25 @@ import sqlite3
 # them with @anvil.server.callable.
 # Here is an example - you can replace it with your own:
 #
+# Defined definitions
+
+parameter_dict = {}
+
 @anvil.server.callable
 def say_hello(name):
   print("Hello, " + name + "!")
   print
-  return 42
+  
+@anvil.server.callable
+def get_config_parameter(test_parameter):
+  new_directory = 'Desktop/SPMP/Database'
+  os.chdir(new_directory)  
+  configuration = read_csv_to_dict('config_parameters.csv')
+  value = pick_parameter_value(configuration, test_parameter)
+  return value
 
+@anvil.server.callable
+def save_configuration(parameter_dict):
+  new_directory = 'Desktop/SPMP/Database'
+  os.chdir(new_directory)
+  write_dict_to_csv('config_parameters.csv',parameter_dict)
