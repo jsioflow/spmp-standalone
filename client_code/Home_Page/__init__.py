6c93faa8ff7f5c84eee761bd248b4aecef0591f9
stone.john@mac.com <stone.john@mac.com>
2024-03-09 09:10:38 +0000

    Edited Home_Page and Settings

--- client_code/Home_Page/__init__.py
+++ client_code/Home_Page/__init__.py
@@ -6,13 +6,17 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-from ..Form1 import Form1
-from ..Form2 import Form2
-from ..Form3 import Form3
+from ..Form1 import Settings
+#from ..Form2 import Form2
+#from ..Form3 import Form3
 
 class Home_Page(Home_PageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
+    # Any code you write here will run when the form opens.
+    anvil.users.login_with_form()
+    print(f'This is a test to see if this is triggered when a user is attempting to get a password')
+    print(f"This user has logged in: {anvil.users.get_user()['email']}")
+    #anvil.server.call('say_hello', 'Anvil Developer')
+    #self.text_box_3.text = 'test_text'
--- client_code/Settings/__init__.py
+++ client_code/Settings/__init__.py
@@ -6,19 +6,11 @@ from anvil.tables import app_tables
 import anvil.server
 import anvil.users
 
-
-
 class Settings(SettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
-# Any code you write here will run when the form opens.
-    anvil.users.login_with_form()
-    print(f'This is a test to see if this is triggered when a user is attempting to get a password')
-    print(f"This user has logged in: {anvil.users.get_user()['email']}")
-    #anvil.server.call('say_hello', 'Anvil Developer')
-    self.text_box_3.text = 'test_text'
+   
     
 
