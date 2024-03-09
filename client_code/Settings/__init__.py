5dc7e715175a849666267121f7a642a7c1f3e0b8
stone.john@mac.com <stone.john@mac.com>
2024-03-09 08:52:48 +0000

    Created Settings; Edited settings; Deleted Form1

--- client_code/Settings/__init__.py
+++ client_code/Settings/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import SettingsTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -6,7 +6,9 @@ from anvil.tables import app_tables
 import anvil.server
 import anvil.users
 
-class Form1(Form1Template):
+
+
+class Settings(SettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
